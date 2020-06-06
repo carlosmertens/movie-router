d66e3d8223c0840bf2e56fb3730477b19da0d9e9
Carlos Mertens <mertenscarlos@yahoo.com>
2020-06-06 21:15:38 +0200

    Map through movies and disply them in DOM

--- src/Home.js
+++ src/Home.js
@@ -28,10 +28,19 @@ class Home extends Component {
   }
 
   render() {
-    console.log(this.state.movieList);
+    // console.log(this.state.movieList);
+    const imageUrl = "http://image.tmdb.org/t/p/w300";
+    const movieGrid = this.state.movieList.map((movie, index) => {
+      return (
+        <div className="col s3" key={index}>
+          <img src={`${imageUrl}${movie.poster_path}`} alt="" />
+        </div>
+      );
+    });
+
     return (
-      <div>
-        <h1>Home</h1>
+      <div className="row">
+        <h1>{movieGrid}</h1>
       </div>
     );
   }
