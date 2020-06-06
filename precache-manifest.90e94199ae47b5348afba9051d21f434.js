3a2cef0d0d5654385a8e29f8c044ce078f27e4ff
Carlos Mertens <mertenscarlos@yahoo.com>
2020-06-07 00:06:28 +0200

    Updates

---
+++ precache-manifest.90e94199ae47b5348afba9051d21f434.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c684d6d05503d3fff665729602b964",
+    "url": "/movie-router/index.html"
+  },
+  {
+    "revision": "2026976c1ddab8ab929b",
+    "url": "/movie-router/static/css/main.dbe8967c.chunk.css"
+  },
+  {
+    "revision": "deff52f7121af7bf8cad",
+    "url": "/movie-router/static/js/2.3e09be0d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-router/static/js/2.3e09be0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2026976c1ddab8ab929b",
+    "url": "/movie-router/static/js/main.c2056606.chunk.js"
+  },
+  {
+    "revision": "1128f7fe3669ccf94f4c",
+    "url": "/movie-router/static/js/runtime-main.ecd79271.js"
+  }
+]);
