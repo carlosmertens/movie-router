d2fbb51685c28eda07e1cb4392ed585162a327d4
Carlos Mertens <mertenscarlos@yahoo.com>
2020-06-06 20:20:14 +0200

    Install Axios and MaterializeCSS

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-function Home(props) {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+      </div>
+    );
+  }
 }
 
 export default Home;
